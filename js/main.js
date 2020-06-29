404b5065e72a5cb588e9172137effb77756846f2
allanbmartins <allanbmartins@yahoo.com>
2020-06-28 23:23:22 -0300

    Segundo commit

---
+++ js/main.js
@@ -0,0 +1,74 @@
+// var nome= "Allan B Martins";
+// var idade= 40;
+// var idade2= 10;
+// var frase= "Japão é o melhor time do mundo";
+// var n1= 5;
+// var n2= 3;
+// // alert(nome + " tem  " + idade + " anos ");
+// // alert(idade + idade2);
+// console.log(nome);
+// console.log(idade + idade2);
+// console.log(frase.replace("Japão","Brasil"));
+// // alert(frase.replace("Japão","Brasil"));
+// // console.log(frase.toUpperCase());
+// console.log(frase.toLowerCase());
+
+// alert(n1 * n2);
+// console.log(n1 / n2);
+
+// var lista = ["maça","pêra", "laranja"];
+// console.log(lista[1]);
+
+// lista.push("uva") //colocar item
+// console.log(lista);
+
+// lista.pop("uva") //tirar item
+// console.log(lista);
+// console.log(lista.length);
+// console.log(lista.reverse());
+
+// console.log(lista[0]);
+// console.log(lista.toString()[0]);
+
+// console.log(lista);
+// console.log(lista.toString());
+// console.log(lista.join(" - "));
+// console.log(lista.join(" | "));
+
+// var fruta = {nome:"maça", cor:"vermelha"}
+// console.log(fruta);
+// console.log(fruta.nome);
+// alert(fruta.cor);
+
+// var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxa"}];
+// console.log(frutas);
+// alert(frutas[1].nome);
+
+// var idade = prompt ("Qual sua idade?")
+// // var idade = 18;
+//     if (idade >= 18){;
+//         alert("Liberado maior de idade");
+// }else{
+//     alert("Proibido menor de idade");
+// };
+
+// var count = 0;
+// while (count <= 5){
+//     console.log(count);
+//     // count = count + 1;
+//     alert(count);
+//     count++;
+// }
+
+// var count = 0;
+// for (count = 1; count <= 5; count++){
+//     alert(count);
+// };
+
+var d = new Date();
+alert(d.getFullYear());
+alert(d.getMonth()+1);
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+
