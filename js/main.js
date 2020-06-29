4cf8871a6c316048e43534ad429077178c08fada
allanbmartins <allanbmartins@yahoo.com>
2020-06-29 17:40:17 -0300

    Terceiro commit

--- js/main.js
+++ js/main.js
@@ -65,10 +65,75 @@
 //     alert(count);
 // };
 
-var d = new Date();
-alert(d.getFullYear());
-alert(d.getMonth()+1);
-alert(d.getDay());
-alert(d.getHours());
-alert(d.getMinutes());
+// var d = new Date();
+// alert(d.getFullYear());
+// alert(d.getMonth()+1);
+// alert(d.getDay());
+// alert(d.getHours());
+// alert(d.getMinutes());
+
+// function soma(n1, n2){
+//     return n1 + n2;
+// }
+
+// function setReplace(frase,nome,novo_nome){
+//     return frase.replace(nome,novo_nome)
+// }
+
+// alert(soma(5, 10));
+// alert(setReplace("Vai Japão","Japão","Brasil")); // Case Sensitive
+
+// function soma(n1, n2){
+//     return n1 + n2;
+// }
+
+// var validar = 0; // Variável global
+// function validaidade(idade){
+//     // var validar; // Variável local
+//     // validar; // não necessário
+//     if (idade >= 18){
+//         validar = true
+//     }else{
+//         validar = false
+//     }
+//     return validar;
+// }
+
+// var idade = prompt("Qual sua idade");
+// console.log(validaidade(idade));
+
+// function clicou(){
+//     alert("Obrigado por clicar");
+// }
+
+function clicou(){
+    document.getElementById("Agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    // console.log(document.getElementById("Agradecimento"));
+}
+
+function redirecionar(){
+    document.getElementById("Vá embora").innerHTML = "<b>Você foi redirecionado? Caso negativo, clique aqui</b>";
+    window.open("https://web.digitalinnovation.one/"); // Abre link em nova página
+    // window.location.href = "https://web.digitalinnovation.one/"; // Abre link na própria página
+}
+
+function trocar(elemento){
+    // document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    // alert("Trocar texto");
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+    // document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    // alert("Trocar texto");
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("Pagina carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
 
